76bcebc339877b3793a1ee2b0377cf1039d713e2
fntysm <zainebb7k9h1@gmail.com>
2022-09-17 10:40:24 +0100

    practice

---
+++ beginning/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+}
+
+
+
+
+
+C:/Users/lenovo/cfc/.git/
+
+
+
+/**other important information**/
+/**"a" ouverture en mode « ajout »
+"r+" ouverture en lecture/écriture
+"w+" création d'un nouveau fichier en lecture/écriture
+"a+" ouverture en mode « ajout » en lecture/écriture
+Si le mode contient le caractère ‘b’ le fichier est ouvert en mode binaire
+Ex : "rb+" ou "r+b" désigne une ouverture en lecture/écriture d’un fichier binaire*/
+
+
+/**- début du fichier, si origine vaut SEEK_SET
+- position courante, si origine vaut SEEK_CUR
+- fin du fichier, si origine vaut SEEK_END*/
+
+
+/**
+c = fgetc( f );
+Lit et retourne (type int) le prochain caractère de f
+Retourne EOF en cas de fin de fichier dépassée ou erreur.
+fputc( c, f );
+Ecrit le caractère c à la position courante du fichier f. Retourne EOF si erreur.
+fgets( buf, n, f );
+Lit dans buf, tous les caractères à partir de la position courante de f, jusqu’à
+trouver une fin de ligne '\n' (qui est aussi lue dans buf) ou alors jusqu’à ce que n-1
+caractères soient lus. Un caractère de fin de chaîne '\0' est rajouté à la fin de buf.
+En cas d'erreur ou fin de fichier, la fonction fgets retourne NULL.
+fputs( buf, f );
+Ecrit tous les caractères contenus de buf (sauf le '\0') dans le fichier f, à partir de
+la position courante. Pour écrire une ligne, il faut prévoir un caractère '\n' à la fin
+de buf (et avant le caractère de fin de chaîne '\0').
+En cas d'erreur, la fonction retourne (type int) EOF.
+Pour les E/S formatées :
+fscanf( f, format, &var1, &var2, ... ) et fprintf( f, format, exp1, exp2, ... )
+
+
+
+ manipulation des fichiers**/
+    /**déclarer une variable fichier f (de type flux)**
+    FILE *fHandler1;
+    FILE *fHandler2;
+    /**pour ouvrir un fichier**
+    mode1 = "r" /**ouverture en lecture*
+    mode2 = "w" /**création d'un nouveau fichier (écrasement de l'ancien s'il existe déjà)*
+    fHandler1 = fopen("file", mode1);
+    fHandler2 = fopen("fileN", mode2);
+    /**schéma typique pour lire séquentiellement le contenu d'un fichier**/
+    /**Pour savoir si on a dépassé la fin de fichier (en mode lecture) feof
+    while(!(feof(fHandler1))){
+        /**le nombre d'éléments lus=fread(le résultat de la lect sera placé dans BUFF,la taille de chaque élément,le nombre d'éléments lus consécutivement,fichier)
+        elementLus = fread()
+        /**le nombre d'éléments écrits=fwrite(récupérés depuis la zone pointée par buf,la taille de chaque élément,le nb d'éléments écrits,fichier)
+        elementEcr = fwrite()
+        /**Pour modifier explicitement la position courante, fseek(fichier,déplace la position courante d'un nombre d'octets relativement lié à, origine)
+        fseek()
+    }
+    /**pour fermer un fichier
+    fclose(fHandler1);
+    fclose(fHandler2);
+*/
