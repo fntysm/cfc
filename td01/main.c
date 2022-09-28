ce7cb6d71d2d87a8469f716528163cb893f5014e
fntysm <zainebb7k9h1@gmail.com>
2022-09-28 20:19:58 +0100

    nbOCC

--- td01/main.c
+++ td01/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 2048
 /**Donner un programme C qui affiche le contenu d'un fichier texte à l'écran.**/
 void lireFichier(){
@@ -56,11 +57,11 @@ FILE* merge2files(char file1[MAX/2], char file2[MAX/2]){
 /**Donner un programme C qui compte le nombre d'occurrence d'un mot donné dans un fichier texte.**/
 int nbOcc(char mot[MAX]){
     FILE *file;
+    char limits[] = "*\t\n,;:.=\"()_{}<>!?&' /";
     char fichier[MAX/2];
-    char buffer[MAX]="";
-    char c;
-    int e;
+    char buffer[MAX];
     int i=0;
+    char *motgot;
     printf("\nentrer le nom du fichier: ");
     scanf("%s",&fichier);
     file = fopen(fichier,"r");
@@ -69,16 +70,16 @@ int nbOcc(char mot[MAX]){
         return -1;
     }
     while(!(feof(file))){
-        c=fgetc(file);
-        printf("%c",c);
-        printf("\nwe before the stncat\n");
-        strncat(buffer, c, 1);
-        printf("\nwe after the stncat\n");
-        if(strcmp(buffer,mot)==0){
-            printf("\nwe've entered here\n");
-            i++;
+            fgets(buffer, MAX,file);
+            motgot = strtok(buffer,limits);
+            while(motgot!=NULL){
+                if(strcmp(motgot,mot)==0){
+                    i++;
+                }
+                 motgot = strtok(NULL, limits);
+            }
+            motgot = strtok(NULL, limits);
         }
-    }
     fclose(file);
     return i;
 }
@@ -86,9 +87,5 @@ int main()
 {
     int d;
     char mot[MAX];
-    printf("\n\nentrer le mot recherche: ");
-    scanf("%s",&mot);
-    d=nbOcc(mot);
-    printf("\n\nthe value of i : %d",d);
     return 0;
 }
