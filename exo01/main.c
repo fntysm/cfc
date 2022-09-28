40bc65551cb3b4f4af47254a0865577a8cff2d96
fntysm <zainebb7k9h1@gmail.com>
2022-09-28 18:16:32 +0100

    ...

--- exo01/main.c
+++ exo01/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX 2048
+/**Donner un programme C qui affiche le contenu d'un fichier texte à l'écran.**/
 int main()
 {
-    printf("Hello world!\n");
+    FILE *file;
+    char buffer[MAX];
+    char fichier[MAX/2];
+    printf("\nentrer le nom du fichier: ");
+    scanf("%s",&fichier);
+    printf("\nle contenu du fichier: \n\n");
+    file = fopen(fichier,"r");
+    while(!(feof(file))){
+        fgets(buffer, MAX,file);
+        printf("%s", buffer);
+    }
+    printf("\n\n");
     return 0;
 }
