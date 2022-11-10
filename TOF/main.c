e369852c1d9676bee09da4b1a01f50f81efdd359
fntysm <zainebb7k9h1@gmail.com>
2022-11-10 10:31:51 +0100

    TOF 02

--- TOF/main.c
+++ TOF/main.c
@@ -3,7 +3,6 @@
 #define MAX 17
 #define MAF 25
 #define b 4 // maximum d'enreg
-
 //les déclarations des structures
 typedef struct enreg{
     char key[MAX]; // la clé
@@ -14,22 +13,23 @@ typedef struct bloc{
     Tenreg Tab[b];
     int nb;
 }Tbloc;
+
 typedef struct Entete
 {
-    int adr_dernier_bloc; //numero d'ordre
-    int nbr_enreg;
-    int indice_libre;
-    int nb_sup;
+    int adr_dernier_bloc; //l'adresse du ernier bloc utilisé
 }Entete;
 
-int entete(TOVC *pF,int i)
+typedef struct TOF
+{
+    FILE *F;
+    Entete entete;
+}TOF;
+
+int entete(TOF *pF,int i)
 {
     if (i==1) return ((pF->entete).adr_dernier_bloc);
-    if (i==2) return ((pF->entete).nbr_enreg);
-    if (i==4) return ((pF->entete).nb_sup);
-    return ((pF->entete).indice_libre);
 }
-void EcrireDir(FILE* pF,int i,Tbloc buf)
+void EcrireDir(TOF *pF,int i,Tbloc buf)
 {
     if (i<=entete(pF,1))
     {
@@ -37,10 +37,14 @@ void EcrireDir(FILE* pF,int i,Tbloc buf)
         fwrite(&buf,sizeof(Tbloc),1,pF->F);
     }
 }
+void aff_entete(TOF *pF,int i,int val)
+{
+    if (i==1) (pF->entete).adr_dernier_bloc=val;
+}
 void chargement_initial(char nomf[MAF], int n, float u){
     // n le nombre d'enreg qu'on veut insérer
     // u désigne le taux de chargement voulu au départ (il est entre 0 et 1)
-    FILE *fh;
+    TOF *fh;
     Tbloc buffer;
     Tenreg e;
     fh = fopen(nomf,"wb");
@@ -50,6 +54,7 @@ void chargement_initial(char nomf[MAF], int n, float u){
     printf("donnez les enregistremens dans l'ordre croissant suivant la clé: ");
     for (k=0;k<n;k++){
         scanf("%s %s",&e.key,&e.info);
+        printf("\nsuccessfully read\n");
         if(j<=(u*b)){
             buffer.Tab[j]=e;
             j++;
@@ -61,10 +66,14 @@ void chargement_initial(char nomf[MAF], int n, float u){
             j=2;
         }
     }
+    buffer.nb = j-1;
+    EcrireDir(fh,i,buffer);
+    aff_entete(fh,1,i);
     fclose(fh);
     return;
 }
 int main()
 {
+    chargement_initial("fichier.bin",3,0.5);
     return 0;
 }
